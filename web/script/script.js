b77150345c884a04437a37a302a04d37c0dbe94c
Bearukun <bearukun@gmail.com>
2016-09-04 02:08:08 +0200

    Small update.

--- web/script/script.js
+++ web/script/script.js
@@ -1,20 +1,39 @@
 window.onload = function () {
+
     var students = [
-        {"name": "Hans", "email": "ha@mail.com", "phone": "29384756", "category" : "Red", "groupname" : "Los Gropos Maximus"},
-        {"name": "Grethe", "email": "gr@mail.com", "phone": "28394056", "category" : "Yellow", "groupname" : "Trunteklubben"},
-        {"name": "Lise", "email": "li@mail.com", "phone": "39483746", "category" : "Green", "groupname" : "Trunteklubben"},
+        {"name": "Hans", "email": "ha@mail.com", "phone": "29384756", "category": "Red", "groupname": "Los Gropos Maximus"},
+        {"name": "Grethe", "email": "gr@mail.com", "phone": "28394056", "category": "Yellow", "groupname": "Trunteklubben"},
+        {"name": "Lise", "email": "li@mail.com", "phone": "39483746", "category": "Green", "groupname": "Trunteklubben"}
     ];
 
     var tableBody = document.getElementById("studentTable");
+
     var populateTable = function () {
         tableBody.innerHTML = "";
         for (var i = 0; i < students.length; i++) {
+
+            //Inserting row
             var row = tableBody.insertRow();
-            row.insertCell(0).innerHTML = students[i].name;
-            row.insertCell(1).innerHTML = students[i].email;
-            row.insertCell(2).innerHTML = students[i].phone;
-            row.insertCell(3).innerHTML = students[i].category;
-            row.insertCell(3).innerHTML = students[i].groupname;
+
+            //Creating the delete button
+            var delButton = document.createElement("button");
+            delButton.className = "btn btn-danger";
+            delButton.id = i + "delButton";
+            delButton.onclick = function () {
+                deleteRow(this.id);
+            };
+
+            var delButtonTxt = document.createTextNode("Delete");
+            delButton.appendChild(delButtonTxt);
+
+            row.insertCell(0).innerHTML = i;
+            row.insertCell(1).innerHTML = students[i].name;
+            row.insertCell(2).innerHTML = students[i].email;
+            row.insertCell(3).innerHTML = students[i].phone;
+            row.insertCell(4).innerHTML = students[i].category;
+            row.insertCell(5).innerHTML = students[i].groupname;
+            row.insertCell(6).appendChild(delButton);
+            
         }
     };
 
@@ -24,8 +43,8 @@ window.onload = function () {
     var studentSelect = document.getElementById("studentSelect");
 
     studentForm.onsubmit = function (event) {
+
         event.preventDefault();
-//        console.log("Firstname: " + studentForm.elements["firstname"].value);
         var student = {};
         student.name = studentForm.elements["name"].value;
         student.email = studentForm.elements["email"].value;
@@ -35,6 +54,22 @@ window.onload = function () {
         students.push(student);
         populateTable();
     };
-    
-};
 
+    var deleteAll = document.getElementById("deleteAll");
+
+    deleteAll.onclick = function (event) {
+        students.length = 0;
+        populateTable();
+        
+    };
+
+    var deleteRow = function (id) {
+        
+        var del = id.substring(0, 1);
+        students.splice(del, 1);
+        populateTable();
+    
+    };
+
+
+};
